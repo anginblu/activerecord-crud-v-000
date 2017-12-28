924d8263a1adc6194e3d4e7608be6760ef003142
Jacqueline Lee <anginblu@gmail.com>
2017-12-28 06:43:11 +0000

    automatically backed up by learn

--- db/migrate/20171228064126_create_movies.rb
+++ db/migrate/20171228064126_create_movies.rb
@@ -2,9 +2,9 @@ class CreateMovies < ActiveRecord::Migration
   def change
     create_table :movies do |t|
       t.string :title
-      t.date :release_date
+      t.integer :release_date
       t.string :director
       t.string :lead
-      in_theaters
+      t.boolean :in_theaters
   end
 end
