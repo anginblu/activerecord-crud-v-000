f04ba272f5a92be7464ba166d7ec4c4aac90c023
Jacqueline Lee <anginblu@gmail.com>
2017-12-28 06:42:50 +0000

    automatically backed up by learn

--- db/migrate/20171228064126_create_movies.rb
+++ db/migrate/20171228064126_create_movies.rb
@@ -1,4 +1,9 @@
 class CreateMovies < ActiveRecord::Migration
   def change
+    create_table :movies do |t|
+      t.string :title
+      t.date :release_date
+      t.string :director
+      t.string :lead, and in_theaters
   end
 end
