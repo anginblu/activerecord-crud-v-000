b1212863861ffb8df0f84c37ee94fbcedefb0588
Jacqueline Lee <anginblu@gmail.com>
2017-12-28 07:23:52 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -81,8 +81,8 @@ end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
-  __
-  __
+  user = User.find_by(name: 'David')
+user.destroy
 end
 
 def can_destroy_all_items_at_once
